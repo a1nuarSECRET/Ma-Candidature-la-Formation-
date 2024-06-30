d694ecc6f1269b7a46d88d276ea1645b38c3cfcb
a1nuarSECRET <109365162+a1nuarSECRET@users.noreply.github.com>
2024-06-30 19:00:44 +0200

    Implementación del nuevo fondo, animaciones y encabezado fijo

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   const name = document.getElementById("name");
   const email = document.getElementById("email");
+  const header = document.querySelector("header");
+  let lastScrollTop = 0;
 
   name.addEventListener("click", function () {
     alert(
@@ -10,5 +12,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
   email.addEventListener("click", function () {
     alert("Vous pouvez me contacter à cette adresse pour plus d'informations.");
+  });
+
+  window.addEventListener("scroll", function () {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop) {
+      header.style.top = "-100px"; // Ocultar encabezado al desplazarse hacia abajo
+    } else {
+      header.style.top = "0"; // Mostrar encabezado al desplazarse hacia arriba
+    }
+    lastScrollTop = scrollTop;
   });
 });
